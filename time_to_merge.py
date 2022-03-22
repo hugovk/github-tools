ad0038c2e027d1827c2de3c5530d44cb62409630
Hugo van Kemenade <hugovk@users.noreply.github.com>
2022-03-22 23:36:45 +0200

    Add 'days since created'

--- time_to_merge.py
+++ time_to_merge.py
@@ -16,8 +16,9 @@ with jsonlines.open("pr_list.jsonl") as reader:
     pull_requests = [line for line in reader]
 # print(f"Total PRs: {len(pull_requests):,}")
 
+today = dt.datetime.today()
 
-print("created_at, merged_at, days to merge")
+print("created_at, merged_at, days to merge, days since created")
 for pr in reversed(pull_requests):
     if not pr["merged_at"]:
         continue
@@ -25,4 +26,9 @@ for pr in reversed(pull_requests):
     timestamp_format = "%Y-%m-%dT%H:%M:%SZ"
     created_at = dt.datetime.strptime(pr["created_at"], timestamp_format)
     merged_at = dt.datetime.strptime(pr["merged_at"], timestamp_format)
-    print(f'{pr["created_at"]}, {pr["merged_at"]}, {(merged_at - created_at).days}')
+    print(
+        f'{pr["created_at"]}, '
+        f'{pr["merged_at"]}, '
+        f"{(merged_at - created_at).days}, "
+        f"{(today - created_at).days}"
+    )
