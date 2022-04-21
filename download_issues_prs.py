cc1a7554d52d6f698c2adf20b006170f72a4c787
Hugo van Kemenade <hugovk@users.noreply.github.com>
2022-04-21 16:23:09 +0300

    Update to download issues as well as PRs

---
+++ download_issues_prs.py
@@ -0,0 +1,63 @@
+"""
+Downloads data for all issues or PRs from a repo and saves them in JSON Lines files
+https://jsonlines.org
+
+Thanks to Ammar Askar
+https://discuss.python.org/t/decision-needed-should-we-close-stale-prs-and-how-many-lapsed-days-are-prs-considered-stale/4637/11?u=hugovk
+"""
+import argparse
+import json
+import os
+
+from github import Github
+
+GITHUB_TOKEN = os.environ["GITHUB_TOKEN"]
+OUTPUT_FILE_ISSUES = "issue_list.jsonl"
+OUTPUT_FILE_PRS = "pr_list.jsonl"
+
+
+def download(label, output_file, repo_get_fn):
+    # Output file is 514 MB for 31,984 PRs!
+    # Output file is 525 MB for 32,611 PRs! (16 mins)
+    # Output file is 387 MB for 91,726 issues! (16 mins)
+    with open(output_file, "w", encoding="utf-8") as f:
+        for i, item in enumerate(repo_get_fn(state="all")):
+            print(f"Retrieved {i} {label}")
+            f.write(json.dumps(item._rawData))
+            f.write("\n")
+
+
+def main():
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument(
+        "-i",
+        "--issues",
+        action="store_true",
+        help="Download issues only. If neither flag set, download PRs and issues.",
+    )
+    parser.add_argument(
+        "-p",
+        "--prs",
+        action="store_true",
+        help="Download PRs only. If neither flag used, download PRs and issues.",
+    )
+    args = parser.parse_args()
+
+    # If neither flag, download both sets
+    if not args.issues and not args.prs:
+        args.issues = args.prs = True
+
+    g = Github(GITHUB_TOKEN, per_page=100)
+    repo = g.get_repo("python/cpython")
+
+    if args.prs:
+        print("Downloading PRs")
+        download("PRs", OUTPUT_FILE_PRS, repo.get_pulls)
+
+    if args.issues:
+        print("Downloading issues")
+        download("issues", OUTPUT_FILE_ISSUES, repo.get_issues)
+
+
+if __name__ == "__main__":
+    main()
