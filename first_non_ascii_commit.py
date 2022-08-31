7f0579f6e7734be89a0119ea9e510f3de73a8087
Hugo van Kemenade <hugovk@users.noreply.github.com>
2022-08-31 13:18:23 +0300

    Count unique authors

--- first_non_ascii_commit.py
+++ first_non_ascii_commit.py
@@ -12,11 +12,13 @@ def print_info(commit: str) -> None:
     print(commit.message)
     print(time.asctime(time.gmtime(commit.committed_date)))
     print(f"https://github.com/python/cpython/commit/{commit}")
+    print()
 
 
 def main() -> None:
     repo = Repo("~/github/cpython")
     non_ascii_commit_messages = non_ascii_authors = 0
+    authors = set()
     for commit in repo.iter_commits("main", reverse=True):
         if not commit.message.isascii():
             if not non_ascii_commit_messages:
@@ -24,18 +26,16 @@ def main() -> None:
                 print_info(commit)
             non_ascii_commit_messages += 1
 
-    print()
-
-    for commit in repo.iter_commits("main", reverse=True):
         if not commit.author.name.isascii():
             if not non_ascii_authors:
                 print("First non-ASCII commit author:\n")
                 print_info(commit)
+            authors.add(commit.author.name)
             non_ascii_authors += 1
 
-    print()
     print("Total non-ASCII commit messages:", non_ascii_commit_messages)
     print("Total non-ASCII commit authors:", non_ascii_authors)
+    print("Unique non-ASCII commit authors:", len(authors))
 
 
 if __name__ == "__main__":
